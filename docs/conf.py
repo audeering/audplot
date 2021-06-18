acc0631cc48449464a778c6030c76476e391cc5d
Hagen Wierstorf <hwierstorf@audeering.com>
2021-06-18 09:40:23 +0200

    Clean up config

--- docs/conf.py
+++ docs/conf.py
@@ -59,9 +59,6 @@ plot_include_source = True
 plot_html_show_source_link = False
 plot_html_show_formats = False
 plot_pre_code = ''
-plot_rcparams = {
-    # 'figure.figsize': '8, 4.5',  # inch
-}
 plot_formats = ['svg']
 
 
