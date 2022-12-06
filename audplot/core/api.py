Fix x-axis scaling for audplot.waveform() (#50)

audplot/core/api.py
@@ -818,11 +818,11 @@ def waveform(
             x_split.append(x[-rest:])
         else:
             x_split = np.split(x, splits)
-        samples = len(x_split)
         x_max = [x.max() for x in x_split]
         x_min = [x.min() for x in x_split]
+        x_axis = (np.arange(len(x_split))) * (samples / len(x_split))
         ax.fill_between(
-            x=range(samples),
+            x=x_axis,
             y1=x_min,
             y2=x_max,
             color=color,

tests/test_api.py
@@ -50,6 +50,10 @@ def test_waveform():
         x = np.ones((2, 100))
         audplot.waveform(x)
     signal = np.random.randn(2000)
-    for n in [400, 800, 1200]:
+    for n in [400, 401, 800, 801, 1200, 1201]:
         audplot.waveform(signal[:n])
+        ax = plt.gca()
+        xlim = ax.get_xlim()
+        assert xlim[0] == 0
+        assert xlim[1] == n
         plt.close()