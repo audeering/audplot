57d108c030a8bc3c7653bf723b9b24ac3e24328f
Hagen Wierstorf <hwierstorf@audeering.com>
2024-04-26 07:53:14 +0200

    Use ruff formatter as pre-commit (#65)
    * Use ruff formatter as pre-commit
    
    * Update usage docs

--- docs/conf.py
+++ docs/conf.py
@@ -7,70 +7,70 @@ import toml
 import audeer
 
 
-config = toml.load(audeer.path('..', 'pyproject.toml'))
+config = toml.load(audeer.path("..", "pyproject.toml"))
 
 
 # Project -----------------------------------------------------------------
-author = ', '.join(author['name'] for author in config['project']['authors'])
-copyright = f'2020-{date.today().year} audEERING GmbH'
-project = config['project']['name']
+author = ", ".join(author["name"] for author in config["project"]["authors"])
+copyright = f"2020-{date.today().year} audEERING GmbH"
+project = config["project"]["name"]
 version = audeer.git_repo_version()
-title = 'Documentation'
+title = "Documentation"
 
 
 # General -----------------------------------------------------------------
-master_doc = 'index'
-source_suffix = '.rst'
+master_doc = "index"
+source_suffix = ".rst"
 exclude_patterns = [
-    'api-src',
-    'build',
-    'tests',
-    'Thumbs.db',
-    '.DS_Store',
+    "api-src",
+    "build",
+    "tests",
+    "Thumbs.db",
+    ".DS_Store",
 ]
-templates_path = ['_templates']
+templates_path = ["_templates"]
 pygments_style = None
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',  # support for Google-style docstrings
-    'sphinx.ext.autosummary',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.intersphinx',
-    'sphinx_autodoc_typehints',
-    'sphinx_copybutton',  # for "copy to clipboard" buttons
-    'sphinxcontrib.katex',  # has to be before jupyter_sphinx
-    'matplotlib.sphinxext.plot_directive',  # include resulting figures in doc
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",  # support for Google-style docstrings
+    "sphinx.ext.autosummary",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.intersphinx",
+    "sphinx_autodoc_typehints",
+    "sphinx_copybutton",  # for "copy to clipboard" buttons
+    "sphinxcontrib.katex",  # has to be before jupyter_sphinx
+    "matplotlib.sphinxext.plot_directive",  # include resulting figures in doc
 ]
 
 # Do not copy prompot output
-copybutton_prompt_text = r'>>> |\.\.\. '
+copybutton_prompt_text = r">>> |\.\.\. "
 copybutton_prompt_is_regexp = True
 
 # Mapping to external documentation
 intersphinx_mapping = {
-    'audmath': ('https://audeering.github.io/audmath/', None),
-    'audmetric': ('https://audeering.github.io/audmetric/', None),
-    'matplotlib': ('https://matplotlib.org/stable/', None),
-    'numpy': ('https://docs.scipy.org/doc/numpy/', None),
-    'pandas': ('https://pandas.pydata.org/pandas-docs/stable/', None),
-    'python': ('https://docs.python.org/3/', None),
-    'scipy': ('https://docs.scipy.org/doc/scipy/', None),
+    "audmath": ("https://audeering.github.io/audmath/", None),
+    "audmetric": ("https://audeering.github.io/audmetric/", None),
+    "matplotlib": ("https://matplotlib.org/stable/", None),
+    "numpy": ("https://docs.scipy.org/doc/numpy/", None),
+    "pandas": ("https://pandas.pydata.org/pandas-docs/stable/", None),
+    "python": ("https://docs.python.org/3/", None),
+    "scipy": ("https://docs.scipy.org/doc/scipy/", None),
 }
 
 # Disable Gitlab as we need to sign in
 linkcheck_ignore = [
-    'https://gitlab.audeering.com',
+    "https://gitlab.audeering.com",
 ]
 
 # Plotting
 plot_include_source = True
 plot_html_show_source_link = False
 plot_html_show_formats = False
-plot_pre_code = ''
+plot_pre_code = ""
 plot_rcparams = {
-    'figure.figsize': '5, 3.8',  # inch
+    "figure.figsize": "5, 3.8",  # inch
 }
-plot_formats = ['svg']
+plot_formats = ["svg"]
 
 # Disable auto-generation of TOC entries in the API
 # https://github.com/sphinx-doc/sphinx/issues/6316
@@ -78,25 +78,24 @@ toc_object_entries = False
 
 
 # HTML --------------------------------------------------------------------
-html_theme = 'sphinx_audeering_theme'
+html_theme = "sphinx_audeering_theme"
 html_theme_options = {
-    'display_version': True,
-    'logo_only': False,
-    'footer_links': False,
+    "display_version": True,
+    "logo_only": False,
+    "footer_links": False,
 }
 html_context = {
-    'display_github': True,
+    "display_github": True,
 }
 html_title = title
 
 
 # Copy API (sub-)module RST files to docs/api/ folder ---------------------
-audeer.rmdir('api')
-audeer.mkdir('api')
-api_src_files = audeer.list_file_names('api-src')
+audeer.rmdir("api")
+audeer.mkdir("api")
+api_src_files = audeer.list_file_names("api-src")
 api_dst_files = [
-    audeer.path('api', os.path.basename(src_file))
-    for src_file in api_src_files
+    audeer.path("api", os.path.basename(src_file)) for src_file in api_src_files
 ]
 for src_file, dst_file in zip(api_src_files, api_dst_files):
     shutil.copyfile(src_file, dst_file)
--- tests/test_api.py
+++ tests/test_api.py
@@ -9,35 +9,35 @@ np.random.seed(1)
 
 
 @pytest.mark.parametrize(
-    'number, expected_string',
+    "number, expected_string",
     [
-        (0, '0'),
-        (1, '1'),
-        (10, '10'),
-        (100, '100'),
-        (1000, '1k'),
-        (10000, '10k'),
-        (100000, '100k'),
-        (1000000, '1M'),
-        (0.1, '100m'),
-        (0.01, '10m'),
-        (0.001, '1m'),
-        (0.0015, '1.5m'),
-        (0.0001, '100u'),
-        (-1, '-1'),
-        (-0.001, '-1m'),
-        (-0.0015, '-1.5m'),
+        (0, "0"),
+        (1, "1"),
+        (10, "10"),
+        (100, "100"),
+        (1000, "1k"),
+        (10000, "10k"),
+        (100000, "100k"),
+        (1000000, "1M"),
+        (0.1, "100m"),
+        (0.01, "10m"),
+        (0.001, "1m"),
+        (0.0015, "1.5m"),
+        (0.0001, "100u"),
+        (-1, "-1"),
+        (-0.001, "-1m"),
+        (-0.0015, "-1.5m"),
         pytest.param(
-            1000 ** 9,
-            '',
+            1000**9,
+            "",
             marks=pytest.mark.xfail(raises=ValueError),
         ),
         pytest.param(
-            1000 ** -4,
-            '',
+            1000**-4,
+            "",
             marks=pytest.mark.xfail(raises=ValueError),
         ),
-    ]
+    ],
 )
 def test_human_format(number, expected_string):
     string = audplot.human_format(number)
